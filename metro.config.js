9627f0e7f889aa82e182422b09b6501258fdb3e3
Ashutosh Jha <ashutoshj887@gmail.com>
2026-01-03 18:45:43 +0530

    feat: add location permissions & update Metro configuration

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,12 @@
-const { getDefaultConfig } = require('expo/metro-config');
-const { withNativeWind } = require('nativewind/metro');
+const { getDefaultConfig } = require('expo/metro-config')
+const { withNativeWind } = require('nativewind/metro')
 
-const config = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname)
 
-module.exports = withNativeWind(config, { input: './global.css' });
+config.resolver.extraNodeModules = {
+  ...config.resolver.extraNodeModules,
+  buffer: require.resolve('buffer'),
+}
+
+module.exports = withNativeWind(config, { input: './global.css' })
 
